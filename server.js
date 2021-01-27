e63065ff517c41530a9ce08a8a2e13d69a189529
Suda Nimh <rslepejian@gmail.com>
2021-01-27 15:53:27 -0800

    removed start attribute

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 
